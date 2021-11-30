7e2e27006b9e8c2cd9c94ccb53ff8e02c339c86b
danielcasto <32224738+danielcasto@users.noreply.github.com>
2021-11-29 20:57:16 -0500

    Added fundamental window components

--- PyBoids/main.py
+++ PyBoids/main.py
@@ -1,7 +1,18 @@
+import pygame as pg, sys
 
 def main():
-    pass
+    pg.init()
+    window_size = 400, 500 # width, height
+    screen = pg.display.set_mode(window_size, pg.RESIZABLE)
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: 
+                sys.exit()
+        
+        screen.fill((0, 0, 0))
+        pg.display.flip()
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
